7308844f6f1f9288c87a9b331cb15eecce24d343
rao-r <rao.r@raor-ltm1.internal.salesforce.com>
2017-07-05 17:26:09 -0700

    Initial Commit

---
+++ WindowManagerSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WindowManagerSample
+//
+//  Created by Raj Rao on 7/5/17.
+//  Copyright © 2017 Raj Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WindowManagerSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WindowManagerSample
+//
+//  Created by Raj Rao on 7/5/17.
+//  Copyright © 2017 Raj Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
